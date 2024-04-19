c956979a12cf1e11f16858481614fab042eaba66
Sand1818 <sandiso.jele@gmail.com>
2024-04-19 23:11:47 +0200

    puppet-lint on Werkzeug package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,8 +2,8 @@
 # Install flask from pip3
 
 package { 'Werkzeug':
- ensure => '2.1.1',
- provider => 'pip3',
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
 package {'flask':
   ensure   => '2.1.0',
