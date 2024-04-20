165b03dfdb60d364be0c597667520e60ff2fbeeb
Sand1818 <sandiso.jele@gmail.com>
2024-04-21 01:32:21 +0200

    advanced 100-puppet_ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,5 +4,5 @@ file {'~/.ssh/config':
 ensure  => 'present',
 path    => '~/.ssh/config',
 mode    => '0644',
-content => 'Host 35.231.236.18\nUser ubuntu\IdentityFile ~/.ssh/school\nPasswordAuthentication no'
+content => 'Host *\nUser ubuntu\IdentityFile ~/.ssh/school\nPasswordAuthentication no'
 }
